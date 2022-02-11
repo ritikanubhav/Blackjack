4473f21a2736ebae8e923d6e6c15bb271351c829
ritikanubhav <ritikanubhav59@gmail.com>
2022-02-11 16:02:02 +0530

    loading corrected

--- main.js
+++ main.js
@@ -9,25 +9,25 @@ let hasBlackjack=false;
 let isAlive=false;
 let message="";
 let messageEl=document.getElementById("message-el");
-let sumEl=document.querySelector("#sum-el");
-let cardsEl=document.getElementById("cards-el");
-let playerEl=document.getElementById("player-el");
-let rulesEl=document.getElementById("rules-el");
+const sumEl=document.querySelector("#sum-el");
+const cardsEl=document.getElementById("cards-el");
+const playerEl=document.getElementById("player-el");
+const rulesEl=document.getElementById("rules-el");
 const cardContainer=document.getElementById("card-container");
-const images=[
-"<img  src='images/cardA.jpg' alt='cardA'>",
-"<img  src='images/card2.jpg' alt='card2'>",
-"<img  src='images/card3.jpg' alt='card3'>",
-"<img  src='images/card4.jpg' alt='card4'>",
-"<img  src='images/card5.png' alt='card5'>",
-"<img  src='images/card6.jpg' alt='card6'>",
-"<img  src='images/card7.jpg' alt='card7'>",
-"<img  src='images/card8.jpg' alt='card8'>",
-"<img  src='images/card9.jpg' alt='card9'>",
-"<img  src='images/card10.jpg' alt='card10'>",
-"<img  src='images/cardJ.png' alt='cardJ'>",
-"<img  src='images/cardK.png' alt='cardK'>",
-"<img  src='images/cardQ.png' alt='cardQ'>",
+const imagesUrl=[
+"images/cardA.jpg",
+"images/card2.jpg",
+"images/card3.jpg",
+"images/card4.jpg",
+"images/card5.png",
+"images/card6.jpg",
+"images/card7.jpg",
+"images/card8.jpg",
+"images/card9.jpg",
+"images/card10.jpg",
+"images/cardJ.png",
+"images/cardK.png",
+"images/cardQ.png",
 ]
 
 function getRandomCard(){
@@ -47,8 +47,10 @@ function rendergame()
         cardlist+=" "+cards[i];
         imagelist+=renderCardImages[i];
     }
-    cardContainer.innerHTML=imagelist;
-    setTimeout(function()
+    cardContainer.innerHTML=`${imagelist}`;
+    let lastCard=document.getElementById(`card${renderCardImages.length}`);
+    console.log(lastCard);
+    lastCard.onload=function()
     {
         rulesEl.style.display="none";
         cardsEl.textContent=cardlist;
@@ -72,15 +74,14 @@ function rendergame()
         messageEl.textContent=message;
         playerEl.textContent=player.name+": $"+player.chips;
 
-    }, 500);
-    
+    };
 }
 
 function startgame(){
     let firstCard=getRandomCard();
     let secondCard=getRandomCard();
-    let card1image=images[firstCard[0]-1];
-    let card2image=images[secondCard[0]-1];
+    let card1image=`<img id="card1" src="${imagesUrl[firstCard[0]-1]}">`;
+    let card2image=`<img id="card2" src="${imagesUrl[secondCard[0]-1]}">`;
     cards=[firstCard[1],secondCard[1]];
     renderCardImages=[card1image,card2image];
     sum=firstCard[1]+secondCard[1];
@@ -93,7 +94,7 @@ function startgame(){
 function newcard(){
     if(isAlive===true&&hasBlackjack===false){
         let newcard=getRandomCard();
-        let newcardimage=images[newcard[0]-1];
+        let newcardimage=`<img id="card${renderCardImages.length+1}" src="${imagesUrl[newcard[0]-1]}">`;
         cards.push(newcard[1]);
         renderCardImages.push(newcardimage);
         sum+=newcard[1];
